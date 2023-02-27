6399b30297e67cfb9cb73f4b9ef9a475792a233e
Mario Wong Pizarroso <st2020424@365.stucom.com>
2023-02-27 18:22:49 +0100

    migrate y seeder hecho (falta seeder de la tabla pivot)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(BooksTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(BookCategoryTableSeeder::class);
     }
 }
